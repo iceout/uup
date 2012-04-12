d2d02ea845472405203d661cf807b9e90aa3567a
iceout <ice.404.out@gmail.com>
2012-04-13 00:50:42 +0800

    use getopt to get parameters

--- ls.c
+++ ls.c
@@ -7,12 +7,47 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <unistd.h>
 
-void do_ls(const char []);
-
-int main(int argc, const char *argv[])
+void do_ls(char []);
+int lsa = 0, lsl = 0;
+int main(int argc, char *argv[])
 {
-    if (argc == 1) {
+    const char* optstring = "al";
+    char *pwd;
+    int opt = 0;
+    int ind = 0;
+    while ((opt = getopt(argc, argv, optstring)) != -1){
+        switch (opt) {
+            case 'a':
+                lsa = 1;
+                printf("lsa = true\n");
+                break;
+            case 'l':
+                lsl = 1;
+                printf("lsl = true\n");
+                break;
+            case '?':
+            default:
+                printf("???\n");
+                break;
+        }
+    }
+    ind = optind;
+    if (ind < argc) {
+        while (ind < argc) {
+            printf("ind == %d\n", ind);
+            do_ls(*(argv+ind));
+            ind++;
+            if (ind != argc) {
+                printf("\n");
+            }
+        }
+    }
+    else {
+        do_ls(".");
+    }
+    /*if (argc == 1) {
         do_ls(".");
     }
     else {
@@ -20,11 +55,11 @@ int main(int argc, const char *argv[])
             printf("%s:\n", * ++argv);
             do_ls(*argv);
         }
-    }
+    }*/
     return 0;
 }
 
-void do_ls(const char dirname[])
+void do_ls(char dirname[])
 {
     DIR *dir_ptr;
     struct dirent *direntp;
